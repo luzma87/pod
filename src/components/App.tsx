078699c1c939085c1d0428d86e2ce391d2b16be3
Luz <luzma_87@yahoo.com>
2018-07-17 08:44:42 -0500

    extracts toolbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,20 +3,12 @@ import * as React from 'react';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-
-import MenuIcon from '@material-ui/icons/Menu';
-import Email from '@material-ui/icons/Email';
-import Info from '@material-ui/icons/Info';
-
 import '../assets/styles/styles.css';
 import { Block } from '../util/types';
 import BlockList from './BlockList';
 import Workspace from './Workspace';
 import InfoDialog from './InfoDialog';
+import MyToolbar from './MyToolbar';
 
 export namespace App {
   export interface Props {
@@ -41,46 +33,13 @@ class App extends React.Component<App.Props, App.State> {
 
   render() {
     const { showInfo } = this.state;
-    const classes = {
-      root: {
-        flexGrow: 1
-      },
-      flex: {
-        flex: 1
-      },
-      menuButton: {
-        marginLeft: -12,
-        marginRight: 20
-      }
-    };
-
     return (
-      <div style={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              style={classes.menuButton}
-              color="inherit"
-              aria-label="Menu"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="title"
-              color="inherit"
-              style={classes.flex}
-            >
-              Design helper
-            </Typography>
-
-            <IconButton onClick={() => this.setState({ showInfo: true })}>
-              <Info color="secondary" />
-            </IconButton>
-            <IconButton onClick={() => window.location.href = 'mailto:project-of-doom@luzma.click'}>
-              <Email color="inherit" />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
+      <div className="flex1">
+        <MyToolbar
+          onInfoClick={() => this.setState({ showInfo: true })}
+          onMenuClick={() => {
+          }}
+        />
         <div className="container">
           <div className="left">
             <BlockList />
---
+++ src/components/MyToolbar.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import Email from '@material-ui/icons/Email';
+import Info from '@material-ui/icons/Info';
+
+export namespace MyToolbar {
+  export interface Props {
+    onInfoClick: () => void
+    onMenuClick: () => void
+  }
+
+  export interface State {
+  }
+}
+
+class MyToolbar extends React.Component<MyToolbar.Props, MyToolbar.State> {
+  render() {
+    const { onInfoClick, onMenuClick } = this.props;
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="Menu"
+            onClick={() => onMenuClick()}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="title"
+            color="inherit"
+            className="flex1"
+          >
+            Design helper
+          </Typography>
+
+          <IconButton onClick={() => onInfoClick()}>
+            <Info color="secondary" />
+          </IconButton>
+          <IconButton
+            color="inherit"
+            onClick={() => window.location.href = 'mailto:project-of-doom@luzma.click'}
+          >
+            <Email />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default MyToolbar;
